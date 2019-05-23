82026b4e278bc3a6c5ec54c958d2d0f192985e2e
Oliver Jones <ogj21@cam.ac.uk>
2019-05-23 12:55:46 +0100

    Added trace of main frequency component in signal

--- console_reader.py
+++ console_reader.py
@@ -48,6 +48,7 @@ def get_data(board, frame_len):
 
 if __name__ == "__main__":
     # log.basicConfig(level=log.DEBUG)
+
     try:
         board = serial.Serial("/dev/ttyACM0", 115200, timeout=5)
 
@@ -62,28 +63,44 @@ if __name__ == "__main__":
         time_stamp = np.linspace(0, len(data_time)/sample_freq, len(data_time))
         print(time_stamp)
         data_freq = np.abs(np.fft.rfft(data_time))
-        fig, (ax1, ax2) = plt.subplots(2)
+        fig, (ax1, ax2, ax3) = plt.subplots(3)
         line1, = ax1.plot(freq_bins, data_freq)
         line2, = ax2.plot(time_stamp, data_time)
         ax1.set_ylim(bottom=0)
         ax1.set_xlim(0, sample_freq/2)
         plt.show(block=False)
 
+        # get note trace
+        index = 0
+        trace_size = 100
+        freq_trace = np.zeros(trace_size)
+        trace_time = np.linspace(0, len(data_time)*trace_size/sample_freq, trace_size)
+        freq_trace[index] = freq_bins[np.argmax(data_freq)]
+        index +=1
+        line3, = ax3.plot(freq_trace, trace_time)
+        ax3.set_xlim(0, sample_freq/2)
+        ax3.set_ylim(0, trace_time.max())
+
         while True:
             data_time = get_data(board, sample_no)
             data_time = data_time - np.mean(data_time)
             data_freq = np.abs(np.fft.rfft(data_time))
+            freq_trace[index] = freq_bins[np.argmax(data_freq)]
+            index = (index +1) % trace_size
             # reset the height if it changes too much
             if data_freq.max() > ax1.get_ylim()[1] or data_freq.max() < ax1.get_ylim()[1]/2:
                 ax1.set_ylim(0, data_freq.max()*1.1)
             if data_time.max() > ax2.get_ylim()[1] or data_time.max() < ax2.get_ylim()[1]/2:
                 ax2.set_ylim(-data_time.max()*1.1, data_time.max()*1.1)
+
+
             line1.set_ydata(data_freq)
             line2.set_ydata(data_time)
+            line3.set_xdata(freq_trace)
             fig.canvas.draw()
             fig.canvas.flush_events()
 
     except Exception as e:
-        print("Caught in exceptio")
+        # board.flush()
         board.close()
         raise e
