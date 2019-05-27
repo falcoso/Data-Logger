ef2b4adaf2d4a9401a128249284f988f432adc2c
Oliver Jones <ogj21@cam.ac.uk>
2019-05-27 09:40:44 +0100

    Switched to pyQTgraph for speed, and added full spectrogram window

--- console_reader.py
+++ console_reader.py
@@ -1,106 +1,156 @@
-import serial
-import matplotlib.pyplot as plt
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtGui, QtCore
 import numpy as np
+import sys
+import serial
 import logging as log
 import struct
 
+class ArduinoBoard:
+    def __init__(self, port, baud, timeout):
+        self.board = serial.Serial(port, baud, timeout=timeout)
+        self.sample_no, self.sample_freq = self.setup()
+        return
 
-def send_command(message, board):
-    message_dict = {"Setup": '0',
-                    "FFT": '1',
-                    "Audio": '2'}
+    def setup(self):
+        line = str(self.board.readline(), "utf-8")
+        if "Setup Complete" not in line:
+            raise RuntimeError("Unable to complete setup, recieved: {}".format(line))
 
-    if message not in message_dict.keys():
-        raise ValueError("Message not configured on Arduino")
+        frame_len = str(self.board.readline(), "utf-8").split(":")
+        frame_len = int(frame_len[-1])
+        log.debug("[+] Sample rate: {}".format(frame_len))
+        sample_freq = str(self.board.readline(), "utf-8").split(":")
+        sample_freq = int(sample_freq[-1])
+        log.debug("[+] Sample rate: {}".format(sample_freq))
+        return frame_len, sample_freq
 
-    # send message
-    board.write(message_dict[message].encode("utf-8"))
-    line = board.readline()
-    log.debug("[+] Message Sent: {} ({})".format(message_dict[message], message))
-    # check confirmation:
-    log.debug("[+] Confirmation recieved: {}".format(line))
-    line = str(line, "utf-8")
-    if message_dict[message] not in line:
-        raise RuntimeError("Unable to acknowledge message:{}".format(line))
-    return
+    def send_command(self, message):
+        message_dict = {"Setup": '0',
+                        "FFT": '1',
+                        "Audio": '2'}
 
+        if message not in message_dict.keys():
+            raise ValueError("Message not configured on Arduino")
 
-def setup(board):
-    line = str(board.readline(), "utf-8")
-    if "Setup Complete" not in line:
-        raise RuntimeError("Unable to complete setup, recieved: {}".format(line))
+        # send message
+        self.board.write(message_dict[message].encode("utf-8"))
+        line = self.board.readline()
+        log.debug("[+] Message Sent: {} ({})".format(message_dict[message], message))
+        # check confirmation:
+        log.debug("[+] Confirmation recieved: {}".format(line))
+        line = str(line, "utf-8")
+        if message_dict[message] not in line:
+            raise RuntimeError("Unable to acknowledge message:{}".format(line))
+        return
 
-    frame_len = str(board.readline(), "utf-8").split(":")
-    frame_len = int(frame_len[-1])
-    log.debug("[+] Sample rate: {}".format(frame_len))
-    sample_freq = str(board.readline(), "utf-8").split(":")
-    sample_freq = int(sample_freq[-1])
-    log.debug("[+] Sample rate: {}".format(sample_freq))
-    return frame_len, sample_freq
+    def get_data(self):
+        data = self.board.read(self.sample_no*2)
+        log.debug(len(data))
+        data = np.array(struct.unpack('h'*self.sample_no, data))
+        return data
 
+class SpectrumAnalyser:
+    def __init__(self):
 
-def get_data(board, frame_len):
-    data = board.read(frame_len*2)
-    log.debug(len(data))
-    data = np.array(struct.unpack('h'*frame_len, data))
-    return data
+        self.board = ArduinoBoard("/dev/ttyACM0", 115200, timeout=5)
+        self.sample_no = self.board.sample_no
+        self.sample_freq = self.board.sample_freq
+        # tell Arduino to start sending data
+        self.board.send_command("Audio")
 
+        # pyqtgraph stuff
+        pg.setConfigOptions(antialias=True)
+        self.traces = dict()
+        self.app = QtGui.QApplication(sys.argv)
+        self.win = pg.GraphicsWindow(title='Spectrum Analyzer')
+        self.win.setWindowTitle('Spectrum Analyzer')
+        # self.win.setGeometry(5, 115, 1910, 1070)
+
+        wf_xaxis = pg.AxisItem(orientation='bottom')
+
+        wf_yaxis = pg.AxisItem(orientation='left')
+
+        sp_xaxis = pg.AxisItem(orientation='bottom')
+
+        self.waveform = self.win.addPlot(
+            title='WAVEFORM', row=1, col=1, axisItems={'bottom': wf_xaxis, 'left': wf_yaxis},
+        )
+        self.spectrum = self.win.addPlot(
+            title='SPECTRUM', row=1, col=2, axisItems={'bottom': sp_xaxis},
+        )
+
+        # waveform and spectrum x points
+        self.x = np.linspace(0, self.sample_no/self.sample_freq, self.sample_no)
+        self.f = np.fft.rfftfreq(self.sample_no, 1/self.sample_freq)
+
+        self.img = pg.ImageItem()
+        self.view = self.win.addViewBox(row=2, col=1, rowspan=2)
+        self.view.addItem(self.img)
+        self.win.addItem(self.view, row=2, col=1, colspan=2)
+
+        self.img_array = np.zeros((1000, int(self.sample_no/2+1)))
+
+        # bipolar colormap
+
+        pos = np.array([0., 1., 0.5, 0.25, 0.75])
+        color = np.array([[0,255,255,255], [255,255,0,255], [0,0,0,255], (0, 0, 255, 255), (255, 0, 0, 255)], dtype=np.ubyte)
+        cmap = pg.ColorMap(pos, color)
+        lut = cmap.getLookupTable(0.0, 1.0, 256)
+
+        self.img.setLookupTable(lut)
+        self.img.setLevels([-50,40])
+
+        yscale = 1.0/(self.img_array.shape[1]/self.f[-1])
+        self.img.scale((1./self.sample_freq)*self.sample_no, yscale)
+        # self.img.setLabel('left', 'Frequency', units='Hz')
+
+    def start(self):
+        if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+            QtGui.QApplication.instance().exec_()
+
+    def set_plotdata(self, name, data_x, data_y):
+        if name in self.traces:
+            self.traces[name].setData(data_x, data_y)
+        else:
+            if name == 'waveform':
+                self.traces[name] = self.waveform.plot(pen='c', width=3)
+                self.waveform.setYRange(-100, 100, padding=0)
+                self.waveform.setXRange(0, self.x.max(), padding=0.005)
+            if name == 'spectrum':
+                self.traces[name] = self.spectrum.plot(pen='m', width=3)
+                # self.spectrum.setLogMode(x=True, y=True)
+                self.spectrum.setYRange(0, 10000, padding=0)
+                self.spectrum.setXRange(0, self.f.max(), padding=0.005)
+
+    def update(self):
+        wf_data = self.board.get_data()
+        wf_data -= int(np.mean(wf_data))
+        self.set_plotdata(name='waveform', data_x=self.x, data_y=wf_data,)
+
+        sp_data = np.abs(np.fft.rfft(wf_data))
+
+        self.set_plotdata(name='spectrum', data_x=self.f, data_y=sp_data)
+
+        self.spectrogram_update(sp_data)
+
+    def spectrogram_update(self, sp_data):
+        # convert to dB
+        psd = 20 * np.log10(sp_data + 0.01*np.ones(len(sp_data)))
+
+        # roll down one and replace leading edge with new data
+        self.img_array = np.roll(self.img_array, -1, 0)
+        self.img_array[-1:] = psd
+
+        self.img.setImage(np.transpose(self.img_array), autoLevels=True)
+
+    def animation(self):
+        timer = QtCore.QTimer()
+        timer.timeout.connect(self.update)
+        timer.start(20)
+        self.start()
 
 if __name__ == "__main__":
     # log.basicConfig(level=log.DEBUG)
-
-    try:
-        board = serial.Serial("/dev/ttyACM0", 115200, timeout=5)
-
-        sample_no, sample_freq = setup(board)
-        # tell Arduino to start sending data
-        send_command("Audio", board)
-
-        # get initial set of data
-        data_time = get_data(board, sample_no)
-        data_time = data_time - np.mean(data_time)
-        freq_bins = np.fft.rfftfreq(len(data_time), 1/sample_freq)
-        time_stamp = np.linspace(0, len(data_time)/sample_freq, len(data_time))
-        print(time_stamp)
-        data_freq = np.abs(np.fft.rfft(data_time))
-        fig, (ax1, ax2, ax3) = plt.subplots(3)
-        line1, = ax1.plot(freq_bins, data_freq)
-        line2, = ax2.plot(time_stamp, data_time)
-        ax1.set_ylim(bottom=0)
-        ax1.set_xlim(0, sample_freq/2)
-        plt.show(block=False)
-
-        # get note trace
-        index = 0
-        trace_size = 100
-        freq_trace = np.zeros(trace_size)
-        trace_time = np.linspace(0, len(data_time)*trace_size/sample_freq, trace_size)
-        freq_trace[index] = freq_bins[np.argmax(data_freq)]
-        index +=1
-        line3, = ax3.plot(freq_trace, trace_time)
-        ax3.set_xlim(0, sample_freq/2)
-        ax3.set_ylim(0, trace_time.max())
-
-        while True:
-            data_time = get_data(board, sample_no)
-            data_time = data_time - np.mean(data_time)
-            data_freq = np.abs(np.fft.rfft(data_time))
-            freq_trace[index] = freq_bins[np.argmax(data_freq)]
-            index = (index +1) % trace_size
-            # reset the height if it changes too much
-            if data_freq.max() > ax1.get_ylim()[1] or data_freq.max() < ax1.get_ylim()[1]/2:
-                ax1.set_ylim(0, data_freq.max()*1.1)
-            if data_time.max() > ax2.get_ylim()[1] or data_time.max() < ax2.get_ylim()[1]/2:
-                ax2.set_ylim(-data_time.max()*1.1, data_time.max()*1.1)
-
-
-            line1.set_ydata(data_freq)
-            line2.set_ydata(data_time)
-            line3.set_xdata(freq_trace)
-            fig.canvas.draw()
-            fig.canvas.flush_events()
-
-    except Exception as e:
-        # board.flush()
-        board.close()
-        raise e
+    audio_app = SpectrumAnalyser()
+    audio_app.animation()
